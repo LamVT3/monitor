498dc5d4ac75801645ff63a0bffdc0a56e9fda32
tungtt6 <tungtt6@topica.edu.vn>
2019-03-07 17:46:46 +0700

    format datetime

--- resources/js/helper/Helper.js
+++ resources/js/helper/Helper.js
@@ -1,6 +1,31 @@
 
 export default class Helper {
+
     static convertTimeStampToDate(timestamp){
-        return new Date(timestamp).toISOString().match(/(\d{4}\-\d{2}\-\d{2})T(\d{2}:\d{2}:\d{2})/)
+
+        let today = new Date(timestamp);
+        let day = today.getDate() + "";
+        let month = (today.getMonth() + 1) + "";
+        let year = today.getFullYear() + "";
+        let hour = today.getHours() + "";
+        let minutes = today.getMinutes() + "";
+        let seconds = today.getSeconds() + "";
+
+        function checkZero(data){
+            if(data.length === 1){
+                data = "0" + data;
+            }
+            return data;
+        }
+
+        day = checkZero(day);
+        month = checkZero(month);
+        year = checkZero(year);
+        hour = checkZero(hour);
+        minutes = checkZero(minutes);
+        seconds = checkZero(seconds);
+
+        return year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
+
     }
 }
